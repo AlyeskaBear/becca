3ce672048a51732e24f403904857ac98ca656b11
Brandon Rohrer <brohrer@gmail.com>
2016-03-26 08:01:47 -0400

    file removed

--- frames_to_movie.py
+++
@@ -1,7 +0,0 @@
-import os 
-import worlds.ffmpeg_tools as ft
-
-world_dir = 'becca_world_chase_ball'
-stills_directory = os.path.join(world_dir, 'frames') 
-movie_filename = 'chase_26.mp4'
-ft.make_movie(stills_directory, movie_filename=movie_filename)
