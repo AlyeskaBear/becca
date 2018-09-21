ddbf2e018a88de12d9f736f8814755465aac7671
Brandon Rohrer <brohrer@fb.com>
2018-09-21 08:35:04 -0400

    Version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='becca',
-    version='0.10.0',
+    version='0.10.1',
     description='A general learning algorithm',
     url='http://github.com/brohrer/becca',
     download_url='https://github.com/brohrer/becca/archive/master.zip',
